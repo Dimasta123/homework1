9276302ffce7156aeb0f67b9c273514e73dc7453
Dimasta123 <60709050+Dimasta123@users.noreply.github.com>
2020-11-11 17:03:37 +0200

    Update app11.js

--- app11.js
+++ app11.js
@@ -3,24 +3,24 @@ const brendy=123.965;
 const headPhones=90.2345;
 const maxPrice =Math.max( soda, brendy, headPhones);
 const minPrice =Math.min( soda, brendy, headPhones);
-const oOsumm =Math.round((Math.floor(soda) + Math.floor(brendy)+ Math.floor(headPhones))/100)*100;
-const summ =  soda + brendy + headPhones;
+const fullPriceRound =Math.round((Math.floor(soda) + Math.floor(brendy)+ Math.floor(headPhones))/100)*100;
+const fullPrice =  soda + brendy + headPhones;
 const moneyBack=(500 - summ ).toFixed(2);
 const avgPrice=(summ/3).toFixed(2);
 
 
 let discount = Math.random();
 let clientDiscount = Math.floor(discount*100);
-let clientPrice = (( 1-discount)*summ ).toFixed(2);
-let fullMoneyGet = ((summ-clientPrice)/2).toFixed(2);
+let clientPrice = (( 1-discount)*fullPrice ).toFixed(2);
+let fullMoneyGet = ((fullPrice-clientPrice)/2).toFixed(2);
 console.log( `максимальне число  ${maxPrice} `);
 console.log( `мінімальне число ${minPrice} `);
 
-console.log( `сума усіх товарів ${summ} `);
+console.log( `сума усіх товарів ${fullPrice} `);
 const roundSumm = Math.floor(soda) + Math.floor(brendy)+ Math.floor(headPhones) ;
 console.log(`сумма без копійок ${roundSumm}`);
 
-console.log(`округленная суммма ${oOsumm}`);
+console.log(`округленная суммма ${fullPriceRound}`);
 const pairNumber = roundSumm%2 == 0;
 console.log(`Булеве зачення ${pairNumber}`);
 
@@ -34,9 +34,9 @@ console.log(`Ціна товарів зі знижкою ${clientPrice}`);
 console.log(`Прибуток ${fullMoneyGet}`);
 console.log(`максимальне число  ${maxPrice};
   мінімальне число ${minPrice};
-  сума усіх товарів ${summ};
+  сума усіх товарів ${fullPrice};
   сумма без копійок ${roundSumm};
-  округленная суммма ${oOsumm};
+  округленная суммма ${fullPriceRound};
   Булеве зачення ${pairNumber};
   Решта при покупці ${moneyBack};
   Середня ціна товару ${avgPrice};
